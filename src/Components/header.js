49bb758d3a049bbbfa10520e58ba9e2a0ad85b4e
Kobe <danrok19@wp.pl>
2023-05-09 13:25:45 +0200

    starting stuff+first version of header+ContextApi

---
+++ src/Components/header.js
@@ -0,0 +1,53 @@
+import { AppBar, Container, MenuItem, Toolbar, Typography, Select, createTheme, ThemeProvider } from "@mui/material"
+import { red } from "@mui/material/colors";
+import { useNavigate } from 'react-router-dom';
+import { CryptoState } from "../CryptoContext";
+
+const styles = {
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "bold",
+        cursor: "pointer"
+    }
+}
+
+
+function Header() {
+    const navigate = useNavigate();
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        }
+    })
+
+
+    const { currency, setCurrency } = CryptoState();
+    //console.log("Current currency is: " + currency);
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color='transparent' position='static'>
+                <Container>
+                    <Toolbar>
+                        <Typography onClick={() => navigate('/')} style={styles.title}>
+                            CryptoApp
+                        </Typography>
+                        <Select variant="outlined" style={{
+                            width: 100, height: 40, marginLeft: 15,color: "white"
+                        }} value={currency} onChange={(event)=> setCurrency(event.target.value)}>
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"PLN"}>PLN</MenuItem>
+                        </Select>
+                    </Toolbar>
+                </Container>
+
+            </AppBar>
+        </ThemeProvider>
+    )
+}
+export default Header;
---
+++ src/CryptoContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+
+const Crypto = createContext();
+
+const CryptoContext = ({children}) => {
+const [currency, setCurrency] = useState("PLN");
+const [symbol, setSymbol] = useState("$$");
+
+
+useEffect(() =>{
+    if( currency === "PLN" ) setSymbol("$$");
+    else if( currency === "USD" ) setSymbol("$");
+},[ currency ])
+
+  return (
+    <Crypto.Provider value={{ currency, symbol, setCurrency}}>
+      {children}
+    </Crypto.Provider>
+  )
+}
+
+export default CryptoContext;
+
+export const CryptoState = () =>{
+    return useContext(Crypto);
+}
